2d3a76f559cd49e25118b388055cfb104d245da3
Anderson Silva <andssilva.23@gmail.com>
2021-09-25 18:01:45 -0400

    feat: :rocket: adiciona word-break na área do usuário

--- src/components/UserArea/style.js
+++ src/components/UserArea/style.js
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  word-break: break-all;
   @media(max-width: 800px) {
     flex-direction: column;
   }
