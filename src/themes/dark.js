979f0f0c00ee970c55402a1c98103e538da25d0d
Anderson Silva <andssilva.23@gmail.com>
2021-09-25 18:26:20 -0400

    feat: :rocket: cria a pasta com temas light e dark

---
+++ src/themes/dark.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  title: 'dark',
+  colors: {
+    primary: '#5300C7',
+    secondary: '#4200A1',
+    text: '#000000B3',
+    textSecondary: '#FFF',
+    background: '#000000B3',
+    border: '#E5E5E5'
+  },
+  fonts: {
+    fontFamily: 'Poppins, sans-serif',
+  }
+}
--- src/themes/light.js
+++ src/themes/light.js
@@ -1,14 +1,15 @@
-const theme = {
+/* eslint-disable */
+export default {
+  title: 'light',
   colors: {
     primary: '#835AFD',
     secondary: '#5B3FB0',
     text: '#000000B3',
-    white: '#FFF',
+    textSecondary: '#FFF',
+    background: '#FFF',
     border: '#E5E5E5'
   },
   fonts: {
     fontFamily: 'Poppins, sans-serif',
   }
-};
-
-export default theme;
+}
